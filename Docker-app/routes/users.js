aade0a07fe0807aa0c4d48b94bf4417b05d0f10a
unknown <newmohib@gmail.com>
2019-12-11 16:34:31 +0600

    get data to another container

--- Docker-app/routes/users.js
+++ Docker-app/routes/users.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 /* GET users listing. */
 router.get('/', (req, res)=> {
 
-  axios.get('http://localhost:4000/users')
+  axios.get('http://product-service-api:4000/users')
   .then(response => {
     console.log(response.data);
     res.send(response.data)
